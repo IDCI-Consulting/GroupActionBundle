61bafe161ee4cbc1f617777ac2de1de704cfd672
Brahim BOUKOUFALLAH <boukoufallah.brahim@gmail.com>
2018-07-17 10:39:44 +0200

    Rework handle chackbowes function.

--- Resources/public/js/src/handleCheckboxes.js
+++ Resources/public/js/src/handleCheckboxes.js
@@ -1,33 +1,37 @@
-export default {
-  handleCheckboxes: function(element) {
-    if (!document.forms['idci_group_action']) {
-      console.warn('The "idci_group_action" was not found !');
+function handleCheckboxes(element) {
+  if (!document.forms['idci_group_action']) {
+    console.warn('The "idci_group_action" was not found !');
 
-      return;
-    }
+    return;
+  }
 
-    var checkboxes = document.forms['idci_group_action'].getElementsByTagName('input');
+  var checkboxes = document.forms['idci_group_action'].getElementsByTagName('input');
 
-    if (element.checked) {
-      checkAll(checkboxes);
-    } else {
-      uncheckAll(checkboxes);
-    }
-  },
+  if (element.checked) {
+    checkAll(checkboxes);
+  } else {
+    uncheckAll(checkboxes);
+  }
+}
 
-  checkAll: function(elements) {
-    for (var i = 0; i < elements.length; i++) {
-      if (elements[i].type == 'checkbox') {
-        elements[i].checked = true;
-      }
-    }
-  },
-
-  uncheckAll: function (elements) {
-    for (var i = 0; i < elements.length; i++) {
-      if (elements[i].type == 'checkbox') {
-        elements[i].checked = false;
-      }
+function checkAll(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].type == 'checkbox') {
+      elements[i].checked = true;
     }
   }
+}
+
+function uncheckAll(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].type == 'checkbox') {
+      elements[i].checked = false;
+    }
+  }
+}
+
+export {
+  handleCheckboxes,
+  checkAll,
+  uncheckAll
 }
